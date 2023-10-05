4e3a9be5e25a3c6bf9f6318b9d8bdaf16069355d
Target-san <target.san@gmail.com>
2023-10-06 01:11:39 +0300

    Initial config parsing

--- src/main.rs
+++ src/main.rs
@@ -65,19 +65,56 @@ fn date_now() -> Date {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Parse command line arguments
-    let _cli = Cli::parse();
+    let Cli { config, command } = Cli::parse();
+    // Fetch path to config file
+    let config_path = if let Some(path) = config {
+        path
+    } else if let Some(path) = dirs::config_dir() {
+        path.join("weather-cli").join("config.toml")
+    } else if let Some(path) = dirs::home_dir() {
+        path.join(".weather-cli.toml")
+    } else {
+        anyhow::bail!(
+            "Current OS doesn't seem to have notion of either user's config directory or user's home directory. Please use explicit '--config' argument"
+        )
+    };
+
+    let config_path = config_path.canonicalize()?;
+    // Read config file itself - if it exists
+    let config = if config_path.is_file() {
+        toml::from_str(&tokio::fs::read_to_string(&config_path).await?)?
+    } else if config_path.exists() {
+        anyhow::bail!(
+            "Path '{}' exists yet points not to file",
+            config_path.display()
+        )
+    } else {
+        toml::Table::new()
+    };
+    // Execute CLI command
+    match command {
+        CliCmd::Configure {
+            provider: _,
+            options: _,
+        } => (),
+        CliCmd::Get {
+            address: _,
+            date: _,
+        } => (),
+        CliCmd::Clear { providers: _ } => (),
+    }
 
     let mut providers = Selector::new();
 
     providers.add_provider::<OpenWeather>("openweather");
     providers.add_provider::<WeatherApi>("weatherapi");
 
-    let config: toml::Value = toml::toml! {
+    let stub_config: toml::Value = toml::toml! {
         apikey = "banana"
     }
     .into();
 
-    let prov = providers.create("weatherapi", config)?;
+    let prov = providers.create("weatherapi", stub_config)?;
 
     let forecast = prov
         .read_weather(
@@ -89,6 +126,15 @@ async fn main() -> anyhow::Result<()> {
         .await?;
 
     println!("{forecast}");
+    // Write config back to file
+    if !config_path.is_file() {
+        // NB: unwrap here is safe, since config path points either to existing file
+        // or to some nonexistent location - so it cannot be just root path
+        // whose parent would be `None`
+        tokio::fs::create_dir_all(config_path.parent().unwrap()).await?;
+    }
 
+    tokio::fs::write(&config_path, toml::to_string_pretty(&config)?).await?;
+    // End of processing
     Ok(())
 }
